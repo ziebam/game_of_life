19d93d0a98caadf2b4247acfc83534933de69a44
ziebam <ziebamichal@tutanota.com>
2020-07-28 19:55:03 +0200

    Refactor the neighbor-counting code
     - Increases efficiency by first checking if the cell is in the middle
       with a try .. except structure. It's faster than always checking
       the exact positiion as most cells are in the middle.
     - If it isn't, there's one function responsible for checking which sides
       the cell is at. It returns tuple for the corners and integer for regular
       sides.
     - The returned types of above-mentioned function are hashable, because
       we need them for a dictionary look-up by which we call the appropriate
       function for the job.
     - Over all this change should improve efficiency by some margin.

--- game_of_life/state_utilities.py
+++ game_of_life/state_utilities.py
@@ -1,238 +1,332 @@
 """Utilities for the state module.
 """
 
-from typing import List
+from typing import List, Hashable
 
 
-def is_top_left_corner(y_pos: int, x_pos: int) -> bool:
-    """Checks whether the cell at a given x and y is the top left corner.
+def which_side(y_pos: int, x_pos: int, width: int, height: int) -> Hashable:
+    """Determines where the cell with given y_pos and x_pos resides.
+
+    Tuples are corners, integers are sides or middle:
+      - 1 is top side,
+      - 2 is right side,
+      - 3 is bottom side,
+      - 4 is left side,
+      - 5 is middle.
 
     Args:
-        y_pos: y-position of the cell.
-        x_pos: x-position of the cell.
-
+        y_pos: y position of the cell.
+        x_pos: x position of the cell.
+        width: Board width.
+        height: Board height.
     Returns:
-        True if the cell is in the top left corner. False otherwise.
+        Hashable later used to determine which function to run via dictionary.
     """
 
-    return x_pos == y_pos == 0
+    # Top left corner.
+    if y_pos == x_pos == 0:
+        side = (1, 4)
+
+    # Top right corner.
+    elif y_pos == 0 and x_pos == width - 1:
+        side = (1, 2)
+
+    # Bottom right corner.
+    elif y_pos == height - 1 and x_pos == width - 1:
+        side = (2, 3)
+
+    # Bottom left corner.
+    elif y_pos == height - 1 and x_pos == 0:
+        side = (3, 4)
+
+    # Top side.
+    elif y_pos == 0:
+        side = 1
+
+    # Right side.
+    elif x_pos == width - 1:
+        side = 2
+
+    # Bottom side.
+    elif y_pos == height - 1:
+        side = 3
+
+    # Left side.
+    elif x_pos == 0:
+        side = 4
+
+    return side
 
 
-def is_top_right_corner(y_pos: int, x_pos: int, width: int) -> bool:
-    """Checks whether the cell at a given x and y is the top right corner.
-
-    Args:
-        y_pos: y-position of the cell.
-        x_pos: x-position of the cell.
-        width: Width of the board.
-
-    Returns:
-        True if the cell is in the top right corner. False otherwise.
-    """
-
-    return x_pos == width - 1 and y_pos == 0
-
-
-def is_bottom_right_corner(y_pos: int, x_pos: int, width: int, height: int) -> bool:
-    """Checks whether the cell at a given x and y is the bottom right corner.
-
-    Args:
-        y_pos: y-position of the cell.
-        x_pos: x-position of the cell.
-        width: Width of the board.
-        height: Height of the board.
-
-    Returns:
-        True if the cell is in the bottom right corner. False otherwise.
-    """
-
-    return x_pos == width - 1 and y_pos == height - 1
-
-
-def is_bottom_left_corner(y_pos: int, x_pos: int, height: int) -> bool:
-    """Checks whether the cell at a given x and y is the bottom left corner.
-
-    Args:
-        y_pos: y-position of the cell.
-        x_pos: x-position of the cell.
-        height: Height of the board.
-
-    Returns:
-        True if the cell is in the bottom left corner. False otherwise.
-    """
-    return x_pos == 0 and y_pos == height - 1
-
-
-def is_top_side(y_pos: int) -> bool:
-    """Checks whether the cell at a given x and y is at the top side.
+def top_left_corner(y_pos: int, x_pos: int, state: List[List[int]]) -> int:
+    """Counts alive neighbors of a cell in the top left corner.
 
     Args:
-        y_pos: y-position of the cell.
-
-    Returns:
-        True if the cell is at the topside. False otherwise.
-    """
-
-    return y_pos == 0
-
-
-def is_right_side(x_pos: int, width: int) -> bool:
-    """Checks whether the cell at a given x and y is at the right side.
-
-    Args:
-        x_pos: x-position of the cell.
-        width: Width of the board.
-
-    Returns:
-        True if the cell is at the right side. False otherwise.
-    """
-
-    return x_pos == width - 1
-
-
-def is_bottom_side(y_pos: int, height: int) -> bool:
-    """Checks whether the cell at a given x and y is at the bottom side.
-
-    Args:
-        y_pos: y-position of the cell.
-        height: Height of the board.
-
-    Returns:
-        True if the cell is in at the bottom side. False otherwise.
-    """
-
-    return y_pos == height - 1
-
-
-def is_left_side(x_pos: int) -> bool:
-    """Checks whether the cell at a given x and y is the bottom right corner.
-
-    Args:
-        x_pos: x-position of the cell.
-
-    Returns:
-        True if the cell is in the bottom right corner. False otherwise.
-    """
-
-    return x_pos == 0
-
-
-def get_alive_neighbors(y_pos: int, x_pos: int, state: List[List]) -> int:
-    """Counts alive neighbors of a cell with the passed coordinates.
-
-    Args:
-        y_pos: y-position of the cell.
-        x_pos: x-position of the cell.
+        y_pos: y position of the cell.
+        x_pos: x position of the cell.
         state: A list of lists representing the board in a 2D space.
 
     Returns:
-        The count of alive neighbors.
+        The count of alive neighbors for a cell in the top left corner.
+    """
+
+    right_neighbor = state[y_pos][x_pos + 1]
+    bottom_right_neighbor = state[y_pos + 1][x_pos + 1]
+    bottom_neighbor = state[y_pos + 1][x_pos]
+
+    neighbors = [right_neighbor, bottom_right_neighbor, bottom_neighbor]
+
+    return neighbors.count(1)
+
+
+def top_right_corner(y_pos: int, x_pos: int, state: List[List[int]]) -> int:
+    """Counts alive neighbors of a cell in the top right corner.
+
+    Args:
+        y_pos: y position of the cell.
+        x_pos: x position of the cell.
+        state: A list of lists representing the board in a 2D space.
+
+    Returns:
+        The count of alive neighbors for a cell in the top right corner.
+    """
+
+    bottom_neighbor = state[y_pos + 1][x_pos]
+    bottom_left_neighbor = state[y_pos + 1][x_pos - 1]
+    left_neighbor = state[y_pos][x_pos - 1]
+
+    neighbors = [bottom_neighbor, bottom_left_neighbor, left_neighbor]
+
+    return neighbors.count(1)
+
+
+def bottom_right_corner(y_pos: int, x_pos: int, state: List[List[int]]) -> int:
+    """Counts alive neighbors of a cell in the bottom right corner.
+
+    Args:
+        y_pos: y position of the cell.
+        x_pos: x position of the cell.
+        state: A list of lists representing the board in a 2D space.
+
+    Returns:
+        The count of alive neighbors for a cell in the bottom right corner.
+    """
+
+    top_left_neighbor = state[y_pos - 1][x_pos - 1]
+    top_neighbor = state[y_pos - 1][x_pos]
+    left_neighbor = state[y_pos][x_pos - 1]
+
+    neighbors = [top_left_neighbor, top_neighbor, left_neighbor]
+
+    return neighbors.count(1)
+
+
+def bottom_left_corner(y_pos: int, x_pos: int, state: List[List[int]]) -> int:
+    """Counts alive neighbors of a cell in the bottom left corner.
+
+    Args:
+        y_pos: y position of the cell.
+        x_pos: x position of the cell.
+        state: A list of lists representing the board in a 2D space.
+
+    Returns:
+        The count of alive neighbors for a cell in the bottom left corner.
+    """
+
+    top_neighbor = state[y_pos - 1][x_pos]
+    top_right_neighbor = state[y_pos - 1][x_pos + 1]
+    right_neighbor = state[y_pos][x_pos + 1]
+
+    neighbors = [top_neighbor, top_right_neighbor, right_neighbor]
+
+    return neighbors.count(1)
+
+
+def top_side(y_pos: int, x_pos: int, state: List[List[int]]) -> int:
+    """Counts alive neighbors of a cell at the top side.
+
+    Args:
+        y_pos: y position of the cell.
+        x_pos: x position of the cell.
+        state: A list of lists representing the board in a 2D space.
+
+    Returns:
+        The count of alive neighbors for a cell at the top side.
+    """
+
+    right_neighbor = state[y_pos][x_pos + 1]
+    bottom_right_neighbor = state[y_pos + 1][x_pos + 1]
+    bottom_neighbor = state[y_pos + 1][x_pos]
+    bottom_left_neighbor = state[y_pos - 1][x_pos - 1]
+    left_neighbor = state[y_pos][x_pos - 1]
+
+    neighbors = [
+        right_neighbor,
+        bottom_right_neighbor,
+        bottom_neighbor,
+        bottom_left_neighbor,
+        left_neighbor,
+    ]
+
+    return neighbors.count(1)
+
+
+def right_side(y_pos: int, x_pos: int, state: List[List[int]]) -> int:
+    """Counts alive neighbors of a cell at the right side.
+
+    Args:
+        y_pos: y position of the cell.
+        x_pos: x position of the cell.
+        state: A list of lists representing the board in a 2D space.
+
+    Returns:
+        The count of alive neighbors for a cell at the right side.
+    """
+
+    top_left_neighbor = state[y_pos - 1][x_pos - 1]
+    top_neighbor = state[y_pos - 1][x_pos]
+    bottom_neighbor = state[y_pos + 1][x_pos]
+    bottom_left_neighbor = state[y_pos + 1][x_pos - 1]
+    left_neighbor = state[y_pos][x_pos - 1]
+
+    neighbors = [
+        top_left_neighbor,
+        top_neighbor,
+        bottom_neighbor,
+        bottom_left_neighbor,
+        left_neighbor,
+    ]
+
+    return neighbors.count(1)
+
+
+def bottom_side(y_pos: int, x_pos: int, state: List[List[int]]) -> int:
+    """Counts alive neighbors of a cell at the bottom side.
+
+    Args:
+        y_pos: y position of the cell.
+        x_pos: x position of the cell.
+        state: A list of lists representing the board in a 2D space.
+
+    Returns:
+        The count of alive neighbors for a cell at the bottom side.
+    """
+
+    top_left_neighbor = top_left_neighbor = state[y_pos - 1][x_pos - 1]
+    top_neighbor = state[y_pos - 1][x_pos]
+    top_right_neighbor = state[y_pos - 1][x_pos + 1]
+    right_neighbor = state[y_pos][x_pos + 1]
+    left_neighbor = state[y_pos][x_pos - 1]
+
+    neighbors = [
+        top_left_neighbor,
+        top_neighbor,
+        top_right_neighbor,
+        right_neighbor,
+        left_neighbor,
+    ]
+
+    return neighbors.count(1)
+
+
+def left_side(y_pos: int, x_pos: int, state: List[List[int]]) -> int:
+    """Counts alive neighbors of a cell at the left side.
+
+    Args:
+        y_pos: y position of the cell.
+        x_pos: x position of the cell.
+        state: A list of lists representing the board in a 2D space.
+
+    Returns:
+        The count of alive neighbors for a cell at the left side.
+    """
+
+    top_left_neighbor = top_left_neighbor = state[y_pos - 1][x_pos - 1]
+    top_neighbor = state[y_pos - 1][x_pos]
+    top_right_neighbor = state[y_pos - 1][x_pos + 1]
+    right_neighbor = state[y_pos][x_pos + 1]
+    left_neighbor = state[y_pos][x_pos - 1]
+
+    neighbors = [
+        top_left_neighbor,
+        top_neighbor,
+        top_right_neighbor,
+        right_neighbor,
+        left_neighbor,
+    ]
+
+    return neighbors.count(1)
+
+
+def middle(y_pos: int, x_pos: int, state: List[List[int]]) -> int:
+    """Counts alive neighbors of a cell in the middle.
+
+    Args:
+        y_pos: y position of the cell.
+        x_pos: x position of the cell.
+        state: A list of lists representing the board in a 2D space.
+
+    Returns:
+        The count of alive neighbors for a cell in the middle.
+    """
+
+    top_left_neighbor = state[y_pos - 1][x_pos - 1]
+    top_neighbor = state[y_pos - 1][x_pos]
+    top_right_neighbor = state[y_pos - 1][x_pos + 1]
+    right_neighbor = state[y_pos][x_pos + 1]
+    bottom_right_neighbor = state[y_pos + 1][x_pos + 1]
+    bottom_neighbor = state[y_pos + 1][x_pos]
+    bottom_left_neighbor = state[y_pos + 1][x_pos - 1]
+    left_neighbor = state[y_pos][x_pos - 1]
+
+    neighbors = [
+        top_left_neighbor,
+        top_neighbor,
+        top_right_neighbor,
+        right_neighbor,
+        bottom_right_neighbor,
+        bottom_neighbor,
+        bottom_left_neighbor,
+        left_neighbor,
+    ]
+
+    return neighbors.count(1)
+
+
+cases = {
+    (1, 4): top_left_corner,
+    (1, 2): top_right_corner,
+    (2, 3): bottom_right_corner,
+    (3, 4): bottom_left_corner,
+    1: top_side,
+    2: right_side,
+    3: bottom_side,
+    4: left_side,
+}
+
+
+def get_alive_neighbors(y_pos: int, x_pos: int, state: List[List[int]]) -> int:
+    """Counts alive neighbors of a cell with the passed coordinates.
+
+    Args:
+        y_pos: y position of the cell.
+        x_pos: x position of the cell.
+        state: A list of lists representing the board in a 2D space.
+
+    Returns:
+        The count of alive neighbors for a cell in state[y_pos][x_pos].
     """
 
     width = len(state[0])
     height = len(state)
 
-    if is_top_left_corner(y_pos, x_pos):
-        right_neighbor = state[y_pos][x_pos + 1]
-        bottom_right_neighbor = state[y_pos + 1][x_pos + 1]
-        bottom_neighbor = state[y_pos + 1][x_pos]
+    # The most cells are in the middle, so short-circuiting the check with
+    # a try .. except is more efficient than checking for the cell position
+    # every time.
+    try:
+        alive_neighbors = middle(y_pos, x_pos, state)
+    except IndexError:
+        position = which_side(y_pos, x_pos, width, height)
+        alive_neighbors = cases[position](y_pos, x_pos, state)
 
-        neighbors = [right_neighbor, bottom_right_neighbor, bottom_neighbor]
-    elif is_top_right_corner(y_pos, x_pos, width):
-        bottom_neighbor = state[y_pos + 1][x_pos]
-        bottom_left_neighbor = state[y_pos + 1][x_pos - 1]
-        left_neighbor = state[y_pos][x_pos - 1]
-
-        neighbors = [bottom_neighbor, bottom_left_neighbor, left_neighbor]
-    elif is_bottom_right_corner(y_pos, x_pos, width, height):
-        top_left_neighbor = state[y_pos - 1][x_pos - 1]
-        top_neighbor = state[y_pos - 1][x_pos]
-        left_neighbor = state[y_pos][x_pos - 1]
-
-        neighbors = [top_left_neighbor, top_neighbor, left_neighbor]
-    elif is_bottom_left_corner(y_pos, x_pos, height):
-        top_neighbor = state[y_pos - 1][x_pos]
-        top_right_neighbor = state[y_pos - 1][x_pos + 1]
-        right_neighbor = state[y_pos][x_pos + 1]
-
-        neighbors = [top_neighbor, top_right_neighbor, right_neighbor]
-
-    elif is_top_side(y_pos):
-        right_neighbor = state[y_pos][x_pos + 1]
-        bottom_right_neighbor = state[y_pos + 1][x_pos + 1]
-        bottom_neighbor = state[y_pos + 1][x_pos]
-        bottom_left_neighbor = state[y_pos - 1][x_pos - 1]
-        left_neighbor = state[y_pos][x_pos - 1]
-
-        neighbors = [
-            right_neighbor,
-            bottom_right_neighbor,
-            bottom_neighbor,
-            bottom_left_neighbor,
-            left_neighbor,
-        ]
-    elif is_right_side(x_pos, width):
-        top_left_neighbor = state[y_pos - 1][x_pos - 1]
-        top_neighbor = state[y_pos - 1][x_pos]
-        bottom_neighbor = state[y_pos + 1][x_pos]
-        bottom_left_neighbor = state[y_pos + 1][x_pos - 1]
-        left_neighbor = state[y_pos][x_pos - 1]
-
-        neighbors = [
-            top_left_neighbor,
-            top_neighbor,
-            bottom_neighbor,
-            bottom_left_neighbor,
-            left_neighbor,
-        ]
-    elif is_bottom_side(y_pos, height):
-        top_left_neighbor = top_left_neighbor = state[y_pos - 1][x_pos - 1]
-        top_neighbor = state[y_pos - 1][x_pos]
-        top_right_neighbor = state[y_pos - 1][x_pos + 1]
-        right_neighbor = state[y_pos][x_pos + 1]
-        left_neighbor = state[y_pos][x_pos - 1]
-
-        neighbors = [
-            top_left_neighbor,
-            top_neighbor,
-            top_right_neighbor,
-            right_neighbor,
-            left_neighbor,
-        ]
-    elif is_left_side(x_pos):
-        top_neighbor = state[y_pos - 1][x_pos]
-        top_right_neighbor = state[y_pos - 1][x_pos + 1]
-        right_neighbor = state[y_pos][x_pos + 1]
-        bottom_right_neighbor = state[y_pos + 1][x_pos + 1]
-        bottom_neighbor = state[y_pos + 1][x_pos]
-
-        neighbors = [
-            top_neighbor,
-            top_right_neighbor,
-            right_neighbor,
-            bottom_right_neighbor,
-            bottom_neighbor,
-        ]
-
-    else:
-        top_left_neighbor = state[y_pos - 1][x_pos - 1]
-        top_neighbor = state[y_pos - 1][x_pos]
-        top_right_neighbor = state[y_pos - 1][x_pos + 1]
-        right_neighbor = state[y_pos][x_pos + 1]
-        bottom_right_neighbor = state[y_pos + 1][x_pos + 1]
-        bottom_neighbor = state[y_pos + 1][x_pos]
-        bottom_left_neighbor = state[y_pos + 1][x_pos - 1]
-        left_neighbor = state[y_pos][x_pos - 1]
-
-        neighbors = [
-            top_left_neighbor,
-            top_neighbor,
-            top_right_neighbor,
-            right_neighbor,
-            bottom_right_neighbor,
-            bottom_neighbor,
-            bottom_left_neighbor,
-            left_neighbor,
-        ]
-
-    return neighbors.count(1)
+    return alive_neighbors
