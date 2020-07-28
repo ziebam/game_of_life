efab7fc30de81ce2bcc7d6789e6aa625ead1a4bb
ziebam <ziebamichal@tutanota.com>
2020-07-28 11:51:20 +0200

    Add a feature to load patterns from file

--- game_of_life/utilities.py
+++ game_of_life/utilities.py
@@ -15,3 +15,14 @@ def clear_screen() -> None:
     """
 
     os.system("cls" if os.name == "nt" else "clear")
+
+
+def load_board_state(file):
+    with open(file) as data:
+        data = data.read().splitlines()
+
+        state = []
+        for row in data:
+            state.append([int(cell) for cell in row])
+
+        return state
